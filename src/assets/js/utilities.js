61bfbce39f34b5cb6b6baf8bd3ecc2f90811c9fa
Benjamin Gray <bendoylegray@gmail.com>
2025-11-07 13:08:12 -0600

    map links to apple maps if the user is on an iOS device

--- src/assets/js/utilities.js
+++ src/assets/js/utilities.js
@@ -1,13 +1,45 @@
-document.addEventListener("DOMContentLoaded", function () {
+function getMobileOS() {
+  const ua = navigator.userAgent;
+  if (/android/i.test(ua)) {
+    return "Android";
+  } else if (
+    /iPad|iPhone|iPod/.test(ua) ||
+    (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1)
+  ) {
+    return "iOS";
+  }
+  return "Other";
+}
+
+function scrollIntoView() {
   const urlParams = new URLSearchParams(window.location.search);
   const parameter = urlParams.get("section");
-
-  const element = document.getElementById(parameter);
+  const element = parameter && document.getElementById(parameter);
 
   if (element) {
     element.scrollIntoView({
       behavior: "smooth",
       block: "center",
     });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  scrollIntoView();
+
+  const map = document.getElementById("contact-page-map-link");
+  if (map) {
+    let mapLink;
+    const osType = getMobileOS();
+
+    if (osType && osType === "iOS") {
+      // If the user is on an iPhone make the map link to Apple maps
+      mapLink =
+        "https://maps.apple.com/directions?destination=199%20Armour%20Dr%20NE%2C%20199%20Armour%20Dr%20NE%2C%20Atlanta%2C%20GA%20%2030324%2C%20United%20States&mode=driving";
+    } else {
+      mapLink = "https://maps.app.goo.gl/kwkqBUiMMPY9Lkp76";
+    }
+
+    map.setAttribute("href", mapLink);
   }
 });
